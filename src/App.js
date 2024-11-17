52ec0615c3a4bf0f0e3d0ce37fd367fb9dc12b8d
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2024-11-17 17:55:33 -0500

    week-lab11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PersonList extends Component {
+
+    //Define state default values
+    state = {
+        persons: []
+    }
+
+     //Component Lifecycle Callback
+    componentDidMount() {
+        axios.get(`https://randomuser.me/api/?results=10`)
+        .then(res => {
+            console.log(res.data);
+            const persons = res.data.results;
+            this.setState({ persons });
+        })
+    }	
+
+    render() {
+        return (
+            <div className="person-list-container" id="person-list-container">
+                <div className="title">
+                    <h1>Person List</h1>
+                </div>
+                {this.state.persons.map((person, index) => (
+                    <div key={index} className="person-card">
+                        <div className="user-name-uuid">
+                            <h3>{`${person.name.title} ${person.name.first} ${person.name.last} - ${person.login.uuid}`}</h3>
+                        </div>
+                        <div className="person-content">
+                            <div className="person-image-container">
+                                <img 
+                                    src={person.picture.large} 
+                                    alt={`${person.name.first} ${person.name.last}`} 
+                                    className="person-image"
+                                />
+                                <button className="detailsbtn">
+                                    Details
+                                </button>
+                            </div>
+                            <div className="person-info">
+                                <p><strong>User Name:</strong> {person.login.username}</p>
+                                <p><strong>Gender:</strong> {person.gender.toUpperCase()}</p>
+                                <p><strong>Time Zone Description:</strong> {person.location.timezone.description}</p>
+                                <p><strong>Address:</strong> {`
+                                ${person.location.street.number} 
+                                ${person.location.street.name}, 
+                                ${person.location.city}, 
+                                ${person.location.state}, 
+                                ${person.location.country} - ${person.location.postcode}
+                                `}</p>
+                                <p><strong>Email:</strong> {person.email}</p>
+                                <p><strong>Birth Date and Age:</strong> {`${new Date(person.dob.date).toISOString()} (${person.dob.age})`}</p>
+                                <p><strong>Register Date:</strong> {new Date(person.registered.date).toISOString()}</p>
+                                <p><strong>Phone#:</strong> {person.phone}</p>
+                                <p><strong>Cell#:</strong> {person.cell}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
-export default App;
+export default PersonList;
